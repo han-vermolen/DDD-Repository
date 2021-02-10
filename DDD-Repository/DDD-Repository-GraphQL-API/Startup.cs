cfd91186872dbfb7969bb74041fa33dcac49d108
han-vermolen <70017892+han-vermolen@users.noreply.github.com>
2021-02-10 16:54:57 +0100

    Initial setup of GraphQL. Fixes #2

---
+++ DDD-Repository/DDD-Repository-GraphQL-API/Startup.cs
@@ -0,0 +1,34 @@
+using DDD_Repository_GraphQL_API.Queries;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DDD_Repository_GraphQL_API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGraphQLServer()
+                .AddQueryType<Query>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapGraphQL(); });
+        }
+    }
+}
